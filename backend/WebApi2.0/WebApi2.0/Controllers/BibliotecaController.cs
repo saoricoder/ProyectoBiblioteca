ffd081384c0e3bf662861fe20f1abb1ed6583798
Betty Rodriguez <blrodriguez3@espe.edu.ec>
2025-02-15 16:00:06 -0500

    biblioteca controller

--- backend/WebApi2.0/WebApi2.0/Controllers/BibliotecaController.cs
+++ backend/WebApi2.0/WebApi2.0/Controllers/BibliotecaController.cs
@@ -1,83 +1,155 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApi2._0.Data.bibioteca_data;
+using WebApi2._0.models.biblioteca;
+
+using System.Collections.Generic;
 
 namespace WebApi2._0.Controllers
 {
+    [ApiController]
+    [Route("/api/[controller]")]
     public class BibliotecaController : Controller
     {
-        // GET: BibliotecaController
-        public ActionResult Index()
+        // GET: api/libros
+        [HttpGet("libros")]
+        public List<Libro_models> GetLibros()
         {
-            return View();
+            return Libro_data.ConsultarLibros();
         }
 
-        // GET: BibliotecaController/Details/5
-        public ActionResult Details(int id)
+        // GET: api/libros/5
+        [HttpGet("libros/{isbn}")]
+        public Libro_models GetLibro(string isbn)
         {
-            return View();
+            return Libro_data.ConsultarLibro(isbn);
         }
 
-        // GET: BibliotecaController/Create
-        public ActionResult Create()
+        // POST: api/libros
+        [HttpPost("libros")]
+        public bool PostLibro([FromBody] Libro_models libro)
         {
-            return View();
+            return Libro_data.InsertarLibro(libro);
         }
 
-        // POST: BibliotecaController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        // PUT: api/libros/5
+        [HttpPut("libros/{isbn}")]
+        public bool PutLibro(string isbn, [FromBody] Libro_models libro)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Libro_data.ActualizarLibro(isbn, libro);
         }
 
-        // GET: BibliotecaController/Edit/5
-        public ActionResult Edit(int id)
+        // DELETE: api/libros/5
+        [HttpDelete("libros/{isbn}")]
+        public bool DeleteLibro(string isbn)
         {
-            return View();
+            return Libro_data.EliminarLibro(isbn);
         }
 
-        // POST: BibliotecaController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        // GET: api/autores
+        [HttpGet("autores")]
+        public List<Autor_models> GetAutores()
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Autor_data.ConsultarAutores();
         }
 
-        // GET: BibliotecaController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: api/autores/5
+        [HttpGet("autores/{codigo}")]
+        public Autor_models GetAutor(int codigo)
         {
-            return View();
+            return Autor_data.ConsultarAutor(codigo);
         }
 
-        // POST: BibliotecaController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        // POST: api/autores
+        [HttpPost("autores")]
+        public bool PostAutor([FromBody] Autor_models autor)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return Autor_data.InsertarAutor(autor);
+        }
+
+        // PUT: api/autores/5
+        [HttpPut("autores/{codigo}")]
+        public bool PutAutor(int codigo, [FromBody] Autor_models autor)
+        {
+            return Autor_data.ActualizarAutor(codigo, autor);
+        }
+
+        // DELETE: api/autores/5
+        [HttpDelete("autores/{codigo}")]
+        public bool DeleteAutor(int codigo)
+        {
+            return Autor_data.EliminarAutor(codigo);
+        }
+
+        // GET: api/prestamos
+        [HttpGet("prestamos")]
+        public List<Prestamo_models> GetPrestamos()
+        {
+            return Prestamos_data.ConsultarPrestamos();
+        }
+
+        // GET: api/prestamos/5
+        [HttpGet("prestamos/{numero}")]
+        public Prestamo_models GetPrestamo(int numero)
+        {
+            return Prestamos_data.ConsultarPrestamo(numero);
+        }
+
+        // POST: api/prestamos
+        [HttpPost("prestamos")]
+        public bool PostPrestamo([FromBody] Prestamo_models prestamo)
+        {
+            return Prestamos_data.InsertarPrestamo(prestamo);
+        }
+
+        // PUT: api/prestamos/5
+        [HttpPut("prestamos/{numero}")]
+        public bool PutPrestamo(int numero, [FromBody] Prestamo_models prestamo)
+        {
+            return Prestamos_data.ActualizarPrestamo(numero, prestamo);
+        }
+
+        // DELETE: api/prestamos/5
+        [HttpDelete("prestamos/{numero}")]
+        public bool DeletePrestamo(int numero)
+        {
+            return Prestamos_data.EliminarPrestamo(numero);
+        }
+
+        // GET: api/detalleprestamos
+        [HttpGet("detalleprestamos")]
+        public List<DetallePrestamo_model> GetDetallePrestamos()
+        {
+            return DetallePrestamos_data.ConsultarDetalles();
+        }
+
+        // GET: api/detalleprestamos/5
+        [HttpGet("detalleprestamos/{id}")]
+        public DetallePrestamo_model GetDetallePrestamo(int id)
+        {
+            return DetallePrestamos_data.ConsultarDetalle(id);
+        }
+
+        // POST: api/detalleprestamos
+        [HttpPost("detalleprestamos")]
+        public bool PostDetallePrestamo([FromBody] DetallePrestamo_model detalle)
+        {
+            return DetallePrestamos_data.InsertarDetalle(detalle);
+        }
+
+        // PUT: api/detalleprestamos/5
+        [HttpPut("detalleprestamos/{id}")]
+        public bool PutDetallePrestamo(int id, [FromBody] DetallePrestamo_model detalle)
+        {
+            return DetallePrestamos_data.ActualizarDetalle(id, detalle);
+        }
+
+        // DELETE: api/detalleprestamos/5
+        [HttpDelete("detalleprestamos/{id}")]
+        public bool DeleteDetallePrestamo(int id)
+        {
+            return DetallePrestamos_data.EliminarDetalle(id);
         }
     }
 }
+
